580ba12b07d09962c56131af3fcb34599200c54e
moneyfacneick <nlyh_97@hotmail.com>
2016-04-27 10:29:20 +0800

    backup

---
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/CameraLean.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLean : MonoBehaviour {
+
+	public float LeanAmount;
+
+	float rotationDamping = 6.0f;
+
+	Waypoints waypointscript;
+	Quaternion lean;
+	// Use this for initialization
+	void Start () {
+		waypointscript = gameObject.GetComponentInParent<Waypoints> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Check if the vehicle is currently turning
+		if (waypointscript.GetUpdateWaypoint()) {
+			Vector3 Direction1, Direction2;
+
+			// Checking which direction the vehicle is turning
+			Direction1 = waypointscript.transform.rotation.eulerAngles;
+			Direction2 = waypointscript.GetObjectRotation().eulerAngles;
+
+			if (Direction1.y < Direction2.y) {
+				LeanRight ();
+			}
+			else if (Direction1.y > Direction2.y) {
+				LeanLeft ();
+			}
+		}
+
+		// If the vehicle is not turning
+		else {
+			lean = Quaternion.identity;
+		}
+
+		// Update the camera to lean in either direction or not lean
+		UpdateRotate ();
+	}
+
+	void UpdateRotate()
+	{
+		// Rotate the camera to lean
+		if (Quaternion.Angle (transform.localRotation, lean) > 2.5f) {
+			Quaternion lerpedQuaternion = Quaternion.Lerp (transform.localRotation, lean, Time.deltaTime * rotationDamping);
+			transform.localRotation = lerpedQuaternion;
+		}
+
+		// Check if the rotate is reached
+		if ((Quaternion.Angle (transform.localRotation, lean) < 2.5f) && transform.localRotation != lean) {
+			transform.localRotation = lean;
+		}
+	}
+
+	void LeanRight()
+	{
+		lean = Quaternion.AngleAxis (LeanAmount, Vector3.forward);
+	}
+
+	void LeanLeft()
+	{
+		lean = Quaternion.AngleAxis (-LeanAmount, Vector3.forward);
+	}
+}
---
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Waypoints : MonoBehaviour {
+
+	public Transform waypoint;
+
+	public int waypointIndex;
+
+	public float currentSpeed = 0.0f;
+
+	List<Transform> waypoints;
+
+	Vector3 direction;
+
+	Quaternion objectRotation;
+
+	Rigidbody body;
+
+	float rotationDamping = 6.0f;
+	float accelaration = 88.0f;
+	float speedLimit = 1774.4f;
+
+	bool updateWaypoint = true;
+
+	// Use this for initialization
+	void Start () {
+		waypointIndex = 0;
+		GetWayPoint ();
+
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Move ();
+	}
+
+	void FixedUpdate() {
+		// Rotate the camera to face the direction of the next waypoint
+		UpdateRotate ();
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		// If collided with a waypoint then switch to the next waypoint
+		if (other.CompareTag("Waypoint")) {
+			ChangeNextWaypoint ();
+		}
+	}
+		
+	void UpdateRotate()
+	{
+		// If there is a waypoint
+		if (waypoint) {
+
+			// If the waypoint have been updated 
+			if (updateWaypoint) {
+
+				// Rotate the camera to view the next waypoint using interpolation
+				body.MoveRotation (Quaternion.Lerp (transform.rotation, objectRotation, Time.deltaTime * rotationDamping));
+
+				// If camera view is looking at the next waypoint stop updating
+				if (Quaternion.Angle (transform.rotation, objectRotation) < 2.5f) {
+					updateWaypoint = false;
+				}
+			}
+		}
+	}
+
+	// Old
+	void Move()
+	{
+		currentSpeed = currentSpeed + accelaration * accelaration;
+
+        if (currentSpeed >= speedLimit)
+        {
+            currentSpeed = speedLimit;
+        }
+
+		Vector3 waypointPosition = waypoint.position;
+		waypointPosition.y = transform.position.y;
+
+		transform.position = Vector3.MoveTowards(transform.position, waypointPosition, Time.deltaTime * currentSpeed);
+	}
+
+	void ChangeNextWaypoint()
+	{
+		// Move to next waypoint
+		waypointIndex++;
+
+		// If have reach the last waypoint go back to the beginning
+		if (waypointIndex >= waypoints.Count) {
+			waypointIndex = 0;
+		}
+
+		// Update the waypoint to the next one
+		waypoint = waypoints [waypointIndex];
+
+		// Calculate the direction of this waypoint to the next one
+		direction = waypoint.position - transform.position;
+		direction.y = 0;
+		direction.Normalize();
+
+		// Calculate the Quaternion using the direction above
+		objectRotation = Quaternion.LookRotation (direction);
+
+		updateWaypoint = true;
+	}
+
+	void GetWayPoint()
+	{
+		Transform getwaypoints = waypoint.GetComponentInChildren<Transform>();
+		waypoints = new List<Transform>();
+
+		// Initing the waypoints into the list
+		foreach (Transform findwaypoints in getwaypoints)
+		{
+			waypoints.Add(findwaypoints);
+		}
+
+		// Update the waypoint
+		waypoint = waypoints [waypointIndex];
+
+		// Calculate the direction of this waypoint to the next one
+		direction = waypoint.position - transform.position;
+		direction.y = 0;
+		direction.Normalize();
+
+		// Calculate the Quaternion using the direction above
+		objectRotation = Quaternion.LookRotation (direction);
+	}
+
+	public bool GetUpdateWaypoint()
+	{
+		return updateWaypoint;
+	}
+
+	public Vector3 GetDirection()
+	{
+		return direction;
+	}
+
+	public Quaternion GetObjectRotation()
+	{
+		return objectRotation;
+	}
+}
